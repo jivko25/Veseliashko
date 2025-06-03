184e2ccdfbd17297bc0c4b1129d228c0f0ade816
Zhivko Uzunov <jivkou@yahoo.com>
2025-06-03 13:03:20 +0300

    add source map

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  vite: {
+    build: {
+      sourcemap: true,  // Включва source maps в продукция
+    }
+  },
 
   app: {
     head: {
