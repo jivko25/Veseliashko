7c7cbd90077239a7f6b5d336ab0c0ec0ef72979f
Zhivko Uzunov <jivkou@yahoo.com>
2025-06-09 11:59:11 +0300

    return previous video

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
                     </RainbowButton>
                 </a>
                 <video autoplay muted loop class="video-bg">
-                    <source src="/videos/6299128-uhd_3840_2160_25fps.webm" type="video/mp4" class="video-bg">
+                    <source src="/videos/6299128-uhd_3840_2160_25fps.mp4" type="video/mp4" class="video-bg">
                 </video>
             </div>
         </section>
